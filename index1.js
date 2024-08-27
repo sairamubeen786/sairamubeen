8cd73072a30e5b3c3fe6ca00fae9808f39e98758
sairamubeen786 <seoseller404@gmail.com>
2024-08-27 11:36:30 -0700

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,5 @@
+const studentId=1234;
+let studentEmail="sairamubeen404@gmail.com";
+var studentPassword="12345";
+studentCity="Layyah";
+console.table([studentId, studentEmail,studentPassword,studentCity ]);
